5a4aeafc83ab6c771805ca781b7bea686c31914f
andrewkhirita <andrei.chirita2013@gmail.com>
2024-10-16 14:24:17 +0300

    test

--- insights/components/home-page/hero.tsx
+++ insights/components/home-page/hero.tsx
@@ -11,7 +11,6 @@ import { Section, Container } from "@/components/craft";
 import { Button } from "@/components/ui/button";
 
 // Asset imports
-// import Logo from "@/public/logo.svg";
 
 const Hero = () => {
   return (
