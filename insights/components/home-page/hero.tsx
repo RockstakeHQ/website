8702042dd266cf8e56d6ddccfc3cc022d6ed8989
andrewkhirita <andrei.chirita2013@gmail.com>
2024-10-17 18:53:13 +0300

    fix header

--- insights/components/home-page/hero.tsx
+++ insights/components/home-page/hero.tsx
@@ -1,36 +1,52 @@
 "use client";
 import React from 'react';
-import Link from "next/link";
-import Balancer from "react-wrap-balancer";
+import { cn } from "@/lib/utils";
 import { Section, Container } from "@/components/craft";
 import { Button } from "@/components/ui/button";
-import { geistExtraBold, geistLight, geistBold, geistMedium, geistMonoRegular, geistMonoBold, geistRegular} from '@/app/fonts';
+import { geistBold, geistMonoRegular, geistMonoSemiBold, geistSemiBold } from '@/app/fonts';
 
 const Hero = () => {
   return (
-    <Section>
-      <Container className="flex flex-col items-center text-center max-w-full px-2 mb-32">
-        <h2 className={`${geistBold.className} text-3xl md:text-4xl lg:text-4xl mb-8 text-white`}>
-          <Balancer>
-            <span className="block md:inline">The Quantum-Betting</span>
-            <span className="block md:inline md:ml-2">Accelerator</span>
-          </Balancer>
+    <Section className="px-0">
+      <Container className="flex flex-col items-center text-center w-full mb-32 px-0 md:px-0">
+        <h2 className={`${geistBold.className} text-4xl md:text-6xl lg:text-7xl xl:text-7xl mb-8 md:mb-6 text-white mt-36 md:mt-42 w-full px-0`}>
+          <span className="block md:inline">The Quantum-Betting Accelerator</span>
         </h2>
-        <h5 className={`${geistMonoRegular.className} text-lg mb-8 text-gray-500 max-w-xl mx-auto`}>
-          <Balancer>
-            Rockstake harnesses state-of-the-art AI to decode 𝕏 sentiment, converting social media buzz into precise betting intelligence.
-          </Balancer>
+        <h5 className={`${geistMonoRegular.className} text-lg md:text-xl mb-16 md:mb-6 text-gray-500 w-full max-w-3xl`}>
+          Rockstake harnesses state-of-the-art AI to decode 𝕏 sentiment, converting social media buzz into precise betting intelligence.
         </h5>
-        <h5 className={`${geistMonoRegular.className} text-sm mb-0 bg-[#090909] text-white px-4 py-2 rounded-full inline-flex items-center border border-[#333333]`}>
+        <h5 className={`${geistMonoSemiBold.className} text-sm mb-2 md:mb-6 bg-[#090909] text-white px-4 py-2 rounded-full inline-flex items-center border border-[#333333]`}>
           <span className="mr-2 text-white bg-[#300037] px-2 py-0.5 rounded-full text-xs font-bold">PRO</span>
           Exclusive content on 𝕏
         </h5>
-        <div className="not-prose mt-6 flex gap-2 md:mt-12">
-          <Button asChild className="bg-gray-800 text-white hover:bg-gray-700">
-            <Link href="/">Explore</Link>
+        <div className="w-full md:w-auto flex flex-col md:flex-row gap-4 mt-4">
+        <Button
+            className={cn(
+              geistSemiBold.className,
+              `bg-[#AC00C2] text-white
+              px-6 py-3 h-12 w-full md:w-48 lg:w-56
+              rounded-lg
+              transition-all duration-200 ease-in-out
+              hover:bg-[#AC00C2]/80 focus:bg-[#AC00C2]/80
+              active:bg-[#AC00C2]/70
+              outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-[#AC00C2]`
+            )}
+          >
+            Subscribe
           </Button>
-          <Button asChild className="bg-[#AC00C2] text-white">
-            <Link href="/posts">Subscribe</Link>
+          <Button
+            className={cn(
+              geistSemiBold.className,
+              `bg-black text-white border border-gray-600
+              px-6 py-3 h-12 w-full md:w-48 lg:w-56
+              rounded-lg
+              transition-all duration-200 ease-in-out
+              hover:bg-white/5 focus:bg-white/5
+              active:bg-white/10
+              outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-600`
+            )}
+          >
+            Explore
           </Button>
         </div>
       </Container>
