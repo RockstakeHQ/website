eb0f8dd6bd2af9a3c66da1fa23d87dd2ec048bf3
andrewkhirita <andrei.chirita2013@gmail.com>
2024-10-17 23:41:21 +0300

    update cta

--- insights/components/home-page/cta.tsx
+++ insights/components/home-page/cta.tsx
@@ -1,75 +1,21 @@
-"use client";
-import * as z from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import Balancer from "react-wrap-balancer";
+import React from 'react';
 import { Section, Container } from "@/components/craft";
-import { Button } from "@/components/ui/button";
-import {
-Form,
-FormControl,
-FormField,
-FormItem,
-FormLabel,
-FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-const formSchema = z.object({
-email: z.string().email({
-message: "Please enter a valid email address.",
-}),
-});
-export function CTA() {
-// 1. Define your form.
-const form = useForm<z.infer<typeof formSchema>>({
-resolver: zodResolver(formSchema),
-defaultValues: {
-email: "",
-},
-});
-// 2. Define a submit handler.
-function onSubmit(values: z.infer<typeof formSchema>) {
-// Do something with the form values.
-// ✅ This will be type-safe and validated.
-console.log(values);
+import { geistSemiBold, geistBold, geistRegular, geistLight, geistBlack } from '@/app/fonts';
+
+
+export function GambleAwareFooter() {
+  return (
+    <Section>
+    <div className="text-white p-4 text-center">
+      <div className="flex items-center justify-center mb-2">
+        <span className="bg-white text-gray-700 text-lg font-bold rounded-full p-1 mr-2">18+</span>
+        <span className={`${geistSemiBold.className} text-xl md:text-2xl font-bold mb-0 md:mb-0`}>BeGambleAware.org</span>
+      </div>
+      <p className={`${geistRegular.className} text-sm md:text-sm text-gray-500`}>Helpline: 0808 8020 133</p>
+      <p className={`${geistBold.className} text-sm md:text-lg text-red-500`}>Please Gamble Responsibly</p>
+    </div>
+    </Section>
+  );
 }
-return (
-<Section>
-<Container className="flex flex-col items-center gap-6 text-center">
-<h2 className="!my-0">Lorem ipsum dolor sit amet!</h2>
-<p className="text-lg opacity-70 md:text-2xl">
-<Balancer>
- Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
- eiusmod tempor incididunt ut labore et dolore magna aliqua.
-</Balancer>
-</p>
-<Form {...form}>
-<form
-onSubmit={form.handleSubmit(onSubmit)}
-className="mt-6 flex h-fit items-center justify-center gap-2"
->
-<FormField
-control={form.control}
-name="email"
-render={({ field }) => (
-<FormItem>
-<FormLabel className="sr-only">Email</FormLabel>
-<FormControl>
-<Input
-className="md:w-64"
-placeholder="Your email address"
-{...field}
-/>
-</FormControl>
-<FormMessage />
-</FormItem>
- )}
-/>
-<Button type="submit">Submit</Button>
-</form>
-</Form>
-</Container>
-</Section>
- );
-}
-export default CTA;
+
+export default GambleAwareFooter;
